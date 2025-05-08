d07304c87b2d1e3d1406c74a88e1e5a6a8e07370
Kaushal Kumar <kk795109@gmail.com>
2025-05-09 02:44:39 +0530

    Label and entries

--- customer.py
+++ customer.py
@@ -1,13 +1,100 @@
 from tkinter import *
+from PIL import Image,ImageTk
+from tkinter import ttk
 
 class Cust_Win:
     def __init__(self, root):
         self.root = root
         self.root.title("Hotel Management System")
-        self.root.geometry("1295x550+0+0")
+        self.root.geometry("1295x550+230+220")
 
 
 
+        #=================title==================
+        lbl_title=Label(self.root,text="ADD CUSTOMER DETAILS",font=("times new roman",18,"bold"),bg="black",fg="gold",bd=4,relief=RIDGE)
+        lbl_title.place(x=0,y=0,width=1295,height=50)
+
+
+        #=================logo==================
+        img2=Image.open(r"D:\Hotel_Management_System\images\logohotel.png")
+        img2=img2.resize((100,40),Image.LANCZOS)
+        self.photoimg2=ImageTk.PhotoImage(img2)
+
+
+        lblimg=Label(self.root,image=self.photoimg2,bd=0,relief=RIDGE)
+        lblimg.place(x=5,y=2,width=100,height=40)
+
+        #=================label frame==================
+        labelframeleft=LabelFrame(self.root,bd=2,relief=RIDGE,text="Customer Details",font=("arial",12,"bold"),padx=2)
+        labelframeleft.place(x=5,y=50,width=425,height=490) 
+
+        #=================label and entries==================
+        #Customer Ref
+        lbl_cust_ref=Label(labelframeleft,text="Customer Ref",font=("arial",12,"bold"),padx=2,pady=6)
+        lbl_cust_ref.grid(row=0,column=0,sticky=W)
+
+        entry_ref=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        entry_ref.grid(row=0,column=1)
+
+        #custname
+        cname=Label(labelframeleft,text="Customer Name",font=("arial",12,"bold"),padx=2,pady=6)
+        cname.grid(row=1,column=0,sticky=W)
+        txtcname=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtcname.grid(row=1,column=1)
+
+        #MotherName
+        lblname=Label(labelframeleft,text="Mother Name",font=("arial",12,"bold"),padx=2,pady=6)
+        lblname.grid(row=2,column=0,sticky=W)
+        txtmname=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtmname.grid(row=2,column=1)
+
+        #gender combobox
+        label_gender=Label(labelframeleft,font=("arial",12,"bold"),text="Gender:",padx=2,pady=6)
+        label_gender.grid(row=3,column=0,sticky=W)
+
+        #postalcode
+        lblPostCode=Label(labelframeleft,text="Postal Code:",font=("arial",12,"bold"),padx=2,pady=6)
+        lblPostCode.grid(row=4,column=0,sticky=W)
+        txtPostCode=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtPostCode.grid(row=4,column=1)
+
+        #mobilenumber
+        lblMobile=Label(labelframeleft,text="Mobile Number:",font=("arial",12,"bold"),padx=2,pady=6)
+        lblMobile.grid(row=5,column=0,sticky=W)
+        txtMobile=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtMobile.grid(row=5,column=1)
+
+        #email
+        lblEmail=Label(labelframeleft,text="Email:",font=("arial",12,"bold"),padx=2,pady=6)
+        lblEmail.grid(row=6,column=0,sticky=W)
+        txtEmail=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtEmail.grid(row=6,column=1)
+
+        #nationality
+        lblNationality=Label(labelframeleft,font=("arial",12,"bold"),text="Nationality:",padx=2,pady=6)
+        lblNationality.grid(row=7,column=0,sticky=W)
+
+        #idproof type combobox
+        lblIdProof=Label(labelframeleft,text="ID Proof Type:",font=("arial",12,"bold"),padx=2,pady=6)
+        lblIdProof.grid(row=8,column=0,sticky=W)
+
+        # id number
+        lblIdNumber=Label(labelframeleft,font=("arial",12,"bold"),text="Id Number:",padx=2,pady=6)
+        lblIdNumber.grid(row=9,column=0,sticky=W)
+        txtIdNumber=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtIdNumber.grid(row=9,column=1)
+
+        # address
+        lblAddress=Label(labelframeleft,font=("arial",12,"bold"),text="Address:",padx=2,pady=6)
+        lblAddress.grid(row=10,column=0,sticky=W)
+        txtAddress=ttk.Entry(labelframeleft,font=("arial",13,"bold"),width=29)
+        txtAddress.grid(row=10,column=1)
+
+
+
+
+
+ 
 
 
 if __name__ == "__main__":
